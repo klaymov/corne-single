ac6ce5ac5dd90d92492cc464c4cf65db82703b4a
klaymov <iiserii98@gmail.com>
2026-07-13 23:11:09 +0300

    style: angle thumb cluster in vial layout to match corne v4.1

--- keymaps/vial/config.h
+++ keymaps/vial/config.h
@@ -3,7 +3,7 @@
 // Згенеруй свій UID командою:
 // python3 -c "import os; print('{' + ', '.join([hex(b) for b in os.urandom(8)]) + '}')"
 // І заміни значення нижче
-#define VIAL_KEYBOARD_UID {0xD1, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88}
+#define VIAL_KEYBOARD_UID {0x91, 0x1A, 0x2B, 0x3C, 0x4D, 0x5E, 0x6F, 0x70}
 
 // Комбо для розблокування (перші дві клавіші — Tab і Q)
 #define VIAL_UNLOCK_COMBO_ROWS {0, 0}
